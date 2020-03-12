50325c7077e9c740534d46393b7a4b0b0dbe7c9e
marina128 <46111183+marina128@users.noreply.github.com>
2020-03-12 18:48:06 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,162 +5,161 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace ConsoleApp2
+namespace ConsoleApp1
 {
-    abstract class Sourse
+    public class Program
     {
-        public string Name { get; set; }
+        public static FileStream  fout = new FileStream("output.txt", FileMode.Create);
+        public static StreamWriter fstr_out = new StreamWriter(fout);
 
-        public string ALastName { get; set; }
-
-        public Sourse(string name, string lname)
-        {
-            Name = name;
-            ALastName = lname;
-        }
-        
-        public virtual void Display()
-        {
-            Console.WriteLine($"Name: {Name}, author's last name: {ALastName}");
-        }
-
-        //определить, являеся ли данное издание искомым
-        public bool Check(string name, string lname)
-        {
-            bool fl = true;
-            if (Name != name)
-                fl = false;
-            if (ALastName != lname)
-                fl = false;
-            return fl;
-        }
-    }
-
-    ///КНИГА
-    class Book : Sourse
-    {
-        public int Year { get; set; }
-        public string PublHouse { get; set; }
-
-        public Book(string name, string lname, int year, string house)
-            : base(name, lname)
-        {
-            Year = year;
-            PublHouse = house;
-        }
-        public override void Display()
-        {
-            Console.WriteLine($"Name: {Name}, author's last name: {ALastName}, year of publishing: {Year}, publishing house {PublHouse}");
-        }
-    }
-    ///ГАЗЕТА
-    class Paper : Sourse
-    {
-        public string Magazine { get; set; }
-        public int MagNum { get; set; }
-        public int Year { get; set; }
-
-        public Paper(string name, string lname, string magazine, int num, int year)
-            : base(name, lname)
-        {
-            Year = year;
-            Magazine = magazine;
-            MagNum = num;
-        }
-
-        public override void Display()
-        {
-            Console.WriteLine($"Name: {Name}, author's last name: {ALastName}, year of publishing: {Year}, name of the magazine: {Magazine}, number of the magazine: {MagNum}");
-        }
-    }
-
-    ///ЭЛЕКТРОН
-    class EResources : Sourse
-    {
-        public string Link { get; set; }
-        public string Annotation { get; set; }
-
-        public EResources(string name, string lname, string link, string annot)
-            : base(name, lname)
-        {
-            Link = link;
-            Annotation = annot;
-        }
-
-        public override void Display()
-        {
-            Console.WriteLine($"Name: {Name}, author's last name: {ALastName}, link: {Link}, annotation: {Annotation}");
-        }
-    }
-
-
-    class Program
-    {
         static void Main(string[] args)
         {
-            FileStream fin;
-            string s, stmp;
-            int n, l, num;
-            List<string> list = new List<string>();
-            Book book;
-            Paper paper;
-            EResources er;
-
-            //созать массив, газету, эл.книгу
-            fin = new FileStream("input.txt", FileMode.Open, FileAccess.Read);
-            StreamReader fstr_in = new StreamReader(fin);
-
-            n = Convert.ToInt32(fstr_in.ReadLine());
-            Sourse[] arr = new Sourse[n];
-
-            for (int i = 0; i < n; i++)
-            {
-                s = fstr_in.ReadLine();
-                l = s.Length;
-                stmp = "";
-
-                for (int j = 0; j < l; j++)
-                {
-                    while (s[j] != ' ' && j < l)
-                    {
-                        stmp += s[j];
-                        j++;
-                    }
-
-                    list.Add(stmp);
-                }
-
-                if (Int32.TryParse(list[2], out num))
-                {
-                    book = new Book(list[0], list[1], num, list[3]);
-                    arr[i] = book;
-                }
-                else if (Int32.TryParse(list[3], out num))
-                {
-                    paper = new Paper(list[0], list[1], list[2], num, Convert.ToInt32(list[4]));
-                    arr[i] = paper;
-                }
-                else
-                {
-                    er = new EResources(list[0], list[1], list[2], list[3]);
-                    arr[i] = er;
-                }
-                list.Clear();
-            }
-
-            fin.Close();
-
-            ///ПОИСК ИЗАНИЯ ПО ФАМИЛИИ
-            Console.WriteLine("Введите фамилию.");
-            string search = Console.ReadLine();
-            for (int i = 0; i < n; i++)
-            {
-                if (arr[i].ALastName == search)
-                    arr[i].Display();
-            }
-
+            string s = Reader();
+            if (s.Length == 0)
+                return;
+            List<float> list = CrList(s);
+            if (list.Count ==0)
+                return;
+            s = FormRes(list);
+            Writer(s);
             Console.ReadKey();
+        }
 
+        /// <summary>
+        /// Считывание строки из файла
+        /// </summary>
+        /// <returns>Считанную строку</returns>
+        public static string Reader()
+        {
+            FileStream fin;
+            string s;
+            try
+            {
+                fin = new FileStream("input.txt", FileMode.Open, FileAccess.Read);
+                StreamReader fstr_in = new StreamReader(fin);
+
+                s = fstr_in.ReadLine();
+
+                fin.Close();
+                //проверка
+                Console.WriteLine(s);
+                if (s == null)
+                    throw new Exception("Файл пустой.");
+                return s;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                fstr_out.Write($"Ошибка: {ex.Message}");
+                fstr_out.Close();
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Запись чисел из строки в список
+        /// </summary>
+        /// <param name="s">Строка с данными из файла</param>
+        /// <returns>Список чисел</returns>
+        public static List<float> CrList(string s)
+        {
+            int l = s.Length;
+            string stmp;
+            List<float> list = new List<float>();
+            try
+            {
+                for (int i = 0; i < l; i++)
+                {
+                    if (s[i] != ' ')
+                    {
+                        stmp = s[i].ToString();
+                        if (i < l - 1)
+                        {
+                            i++;
+                            if (stmp == "0" && s[i] != ' ')
+                                throw new Exception("Входная строка имела неверный формат.");
+                            while (s[i] != ' ' && i < l - 1)
+                            {
+                                stmp += s[i];
+                                i++;
+                            }
+                            if (i == l - 1)
+                                stmp += s[i];
+                        }
+                        
+                        list.Add(float.Parse(stmp.Replace('.', ',')));
+                    }
+                }
+                if (list.Count == 0)
+                    throw new Exception("Входная строка имела неверный формат.");
+                list.Sort();
+                return list;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                fstr_out.Write($"Ошибка: {ex.Message}");
+                fstr_out.Close();
+                list.Clear();
+                return list;
+            }
+        }
+
+        /// <summary>
+        /// Запись повторяющихся чисел в строку
+        /// </summary>
+        /// <param name="list">Список чисел</param>
+        /// <returns>Ничего не возвращает</returns>
+        public static string FormRes(List<float> list)
+        {
+            //удаление лишних значений
+            for (int i = 0; i < list.Count; i++)        
+            {
+                if (i < list.Count - 1 && list[i] == list[i + 1])
+                {
+                    i++;
+                    while (i < list.Count - 1 && list[i] == list[i + 1])
+                    {
+                        list.RemoveAt(i);
+                    }
+                }
+                list.RemoveAt(i);
+                i--;
+            }
+
+            //запись в строку 
+            string res = "";
+            for (int i = 0; i < list.Count; i++)
+            {
+                res += list[i].ToString() + " ";
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Считывание строки из файла
+        /// </summary>
+        /// <param name="res">Строка, с результатом</param>
+        /// <returns>Считанную строку</returns>
+        public static void Writer(string res)
+        {
+            try
+            {
+                //проверка
+                Console.WriteLine(res);
+
+                fstr_out.Write(res);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                fstr_out.Write($"Ошибка: {ex.Message}");
+                fstr_out.Close();
+                return;
+            }
+
+            fstr_out.Close();
         }
     }
 }
